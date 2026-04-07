773e3997f77a128f5b7b19bc0302a8525d14b419
Your Name <you@example.com>
2026-04-07 23:31:36 +0100

    bac

--- project/edx-helper/bash/.lib/bac.sh
+++ project/edx-helper/bash/.lib/bac.sh
@@ -2,6 +2,12 @@ bacDir=~
 bacName=`basename $venv`
 
 backup_build(){
+	echo $bacDir
+	echo $bacName
+	echo $venv
+}
+
+abackup_build(){
 	if [ ! -f $bacDir/$bacName.tgz ];then
 		tar cfvz $bacDir/$bacName.tgz $venv
 	fi
