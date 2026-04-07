75a5fe467d61b1d3615def2921af73857c533eae
Your Name <you@example.com>
2026-04-07 22:26:32 +0100

    ex

--- project/edx-helper/bash/.lib/build.sh
+++ project/edx-helper/bash/.lib/build.sh
@@ -1,4 +1,7 @@
 requirements(){
-	cd $venv/$name
-	pip install -r requirements.txt
+	check="$venv/lib/python3.11/site-packages/beautifulsoup4-*-info"
+	if [ ! -d $check ];then
+		cd $venv/$name
+		pip install -r requirements.txt
+	fi
 }
